d59e4556aa62a644df6755bd00b37ffd94c859aa
vdphan <673@holbertonschool.com>
2019-06-28 01:05:52 +0000

    update base model with dict and kwargs in question 4

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,19 @@ from datetime import datetime
 
 class BaseModel():
     """class BaseModel that defines all attributes/methods for other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initialize args, kwargs, id, created_at, updated_at"""
-        self.id = str(uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        if not kwargs or len(kwargs) == 0:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+        else:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    cv = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, cv)
+                elif key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """return a string"""
