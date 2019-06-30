db6e251f2aa739ab8b392c1092abc4dacf5d008d
snehadasa <sneha.dasasneha@gmail.com>
2019-06-29 18:41:03 -0700

    sub class inherited from super class BaseModel

---
+++ models/user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class User that inherits from BaseModel"""
+
+
+class User(BaseModel):
+    """Sub class User that inherits from super class BaseModel"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
