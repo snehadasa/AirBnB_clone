333b5f73e7f9487ba810de2eabecd7f1e41627fd
vdphan <673@holbertonschool.com>
2019-07-01 08:42:06 +0000

    add all

--- console.py
+++ console.py
@@ -11,6 +11,7 @@ from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """class HBNBCommand the interpreter"""
+    name = ["BaseModel", "User"]
 
     def do_quit(self, args):
         """Quit command to exit the program"""
@@ -39,12 +40,11 @@ class HBNBCommand(cmd.Cmd):
         Prints the string representation of an instance
         based on the class name and id
         """
-        a = ["BaseModel", "User"]
         if not cls:
             print("** class name missing **")
             return
         l = cls.split()
-        if l[0] not in a:
+        if l[0] not in HBNBCommand.name:
             print("** class doesn't exist **")
         elif len(l) < 2:
             print("** instance id missing **")
@@ -57,7 +57,42 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, cls=None):
         """Deletes an instance based on the class name and id"""
-        
+        if not cls:
+            print("** class name missing **")
+            return
+        l = cls.split()
+        if l[0] not in HBNBCommand.name:
+            print("** class doesn't exist **")
+        elif len(l) < 2:
+            print("** instance id missing **")
+        else:
+            key = l[0] + "." + l[1]
+            if key in storage.all():
+                del storage.all()[key]
+                storage.save()
+                print(storage.all())
+            else:
+                print("** no instance found **")
+
+    def do_all(self, cls=None):
+        """
+        Prints all string representation of all instances based
+        or not on the class name
+        """
+        l = []
+        d = storage.all()
+        if not cls:
+            for value in d.values():
+                l.append(str(value))
+            print(l)
+        else:
+            if cls in HBNBCommand.name:
+                for key, value in d.items():
+                    if key.split(".")[0] == cls:
+                        l.append(str(value))
+                print(l)
+            else:
+                print("** class doesn't exist **")
 
 if __name__ == "__main__":
     prompt = HBNBCommand()
