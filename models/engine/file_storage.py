749b2f09f15d9cb9d17937865809bb7019315de4
snehadasa <sneha.dasasneha@gmail.com>
2019-07-01 10:52:29 -0700

    starting with test cases

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,7 +5,6 @@
 import json
 import os.path
 from models.base_model import BaseModel
-from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
